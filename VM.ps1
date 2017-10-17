4e68b5605cbce48d9e40c40ea506be7713bcc692
zigmax <zig_max@hotmail.com>
2017-10-17 19:44:27 -0400

    Update VM.ps1

--- VM.ps1
+++ VM.ps1
@@ -6,10 +6,10 @@
 New-AzureRmResourceGroup -Name RG-TEST -Location "West US 2"
 
 #-- Tester avant de deployer ---
-Test-AzureRmResourceGroupDeployment -ResourceGroupName RG-Test -TemplateFile .\azuredeployVMPassVault.json -TemplateParameterFile .\azuredeployVMPassVault.parameters.json
+Test-AzureRmResourceGroupDeployment -ResourceGroupName RG-Test -TemplateFile .\azuredeployVM.json -TemplateParameterFile .\azuredeployVM.parameters.json
 
 #-- Deployer la VM avec mot de passe stocke dans Azure Key Vault a partir d'un modele ARM et d'un fichier de parametre contenant le chemin vers le secret dans le Vault
-New-AzureRmResourceGroupDeployment -ResourceGroupName RG-Test -TemplateFile .\azuredeployVMPassVault.json -TemplateParameterFile .\azuredeployVMPassVault.parameters.json
+New-AzureRmResourceGroupDeployment -ResourceGroupName RG-Test -TemplateFile .\azuredeployVM.json -TemplateParameterFile .\azuredeployVM.parameters.json
 
 
 ##-- FlushAll
